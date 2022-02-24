d19d01e5cc7749991132f7932c6b61e9a3da7dd0
Tothenwe <99398257+Tothenwe@users.noreply.github.com>
2022-02-24 10:02:16 +0530

    Add files via upload

---
+++ multi-module2/Serviceapp/src/main/java/com/MIN/Src1.java
@@ -0,0 +1,10 @@
+package com.MIN;
+
+public class Src1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("bye!!");
+	}
+
+}
---
+++ multi-module2/Serviceapp/src/test/java/com/MIN/Src1Test.java
@@ -0,0 +1,14 @@
+package com.MIN;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Src1Test {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
